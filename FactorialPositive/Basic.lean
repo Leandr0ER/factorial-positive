6d6915bb3a684b0eb92aee609eb117a6dcd5d651
Leandro Estrada R <lestradar@uni.edu.pe>
2026-05-07 20:51:25 -0300

    feat: add factorial definition and positivity proof with blueprint linking

--- FactorialPositive/Basic.lean
+++ FactorialPositive/Basic.lean
@@ -1,1 +1,13 @@
-def hello := "world"
+import Mathlib.Data.Nat.Basic
+
+def fac : ℕ →  ℕ 
+  | 0 => 1
+  | n+1 => (n+1) * fac n
+
+theorem fac_pos (n: ℕ) : 0 < fac n := by
+  induction n with
+  | zero =>
+    simp [fac]
+  | succ n ih =>
+    unfold fac
+    exact Nat.mul_pos (Nat.succ_pos n) ih
